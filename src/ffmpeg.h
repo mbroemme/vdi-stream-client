02bdece2b04c3cc3c834892c3c56748785d20932
Maik Broemme <mbroemme@libmpq.org>
2026-05-12 16:10:53 +0200

    docs(video): add FFmpeg header license exception notice
    Add the GPLv3 section 7 additional-permission notice to ffmpeg.h.
    
    This keeps the new FFmpeg decoder header consistent with the implementation
    file and documents that the COPYING.EXCEPTION permission applies when linking
    with the Parsec SDK.

--- src/ffmpeg.h
+++ src/ffmpeg.h
@@ -7,6 +7,9 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
+ *
+ *  Additional permission under GNU GPL version 3 section 7 is described in
+ *  COPYING.EXCEPTION, allowing this program to link with the Parsec SDK.
  */
 
 #ifndef _FFMPEG_H
